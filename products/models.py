2d9cdca139becb6c83ba6dbb59421f04caf015e5
sameet <sameetzala@gmail.com>
2021-02-25 19:25:33 +0530

    done alote of changes

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from accounts.models import CustomUser
+from datetime import timezone
 # Create your models here.
 class Product(models.Model):
 
@@ -13,3 +14,25 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+class Cart(models.Model):
+    
+    cart_id = models.AutoField(primary_key=True)
+    user = models.OneToOneField(CustomUser, on_delete= models.CASCADE)
+    # created_on = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.user.first_name
+
+class ProductInCart(models.Model):
+  
+    class Meta:
+        unique_together = (('cart','product'),)
+    product_in_cart_id = models.AutoField(primary_key=True)
+    cart = models.ForeignKey(Cart,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    
+    def __str__(self):
+        return self.product.product_name
+
--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path,include
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index,name='index'),
+    path('addtocart/<int:id>/', views.addToCart,name='addtocart'),
+    path('displaycart/', views.displaycart,name='displaycart'),
+    path('deletefromcart/<int:id>/', views.deletefromcart,name='deletefromcart'),
+    path('viewproduct/<int:id>/', views.viewproduct,name='viewproduct'),
 
 ]
