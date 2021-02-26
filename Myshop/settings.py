4628176d8e5abb00023c5c81a584061d4c9d02d4
sameet <sameetzala@gmail.com>
2021-02-27 00:01:40 +0530

    d

--- Myshop/settings.py
+++ Myshop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'p$oj!^c^2!uqyd#e0=txmltdydp8lgtvh3v9$z86l1o$$&xl7j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['my-shoop.herokuapp.com']
 # ALLOWED_HOSTS = []
