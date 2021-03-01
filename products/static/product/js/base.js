df652adcd21918d45bae510d2777e78445f8b3ba
sameet <sameetzala@gmail.com>
2021-03-01 15:17:12 +0530

    hamburgerbar e

--- products/static/product/js/base.js
+++ products/static/product/js/base.js
@@ -1,8 +1,14 @@
-const hamburger = document.getElementById('bars');
-const navUL = document.getElementById('nav');
+// const hamburger = document.getElementById('bars');
+// const navUL = document.getElementById('nav');
 
-hamburger.addEventListener('click',() =>{
-    navUL.classList.toggle('show');
-    navUL.classList.toggle('change-bg');
+// hamburger.addEventListener('click',() =>{
+//     navUL.classList.toggle('show');
+//     navUL.classList.toggle('change-bg');
     
-})
+// })
+
+function onClickMenu(){
+    document.getElementById('nav').classList.toggle('show');
+    document.getElementById('nav').classList.toggle('change-bg');
+
+}
