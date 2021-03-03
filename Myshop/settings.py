a823b3007f89422f302e8ebde457f28ed310752c
sameet <sameetzala@gmail.com>
2021-03-03 15:46:10 +0530

    did some normal changes

--- Myshop/settings.py
+++ Myshop/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT =  os.path.join(BASE_DIR,'staticfiles')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'products/static')
 # MEDIA_URL = '/staticfiles/'
